baaa6fa3aa7274e8d8b1d8f3ff687f344a7fe672
Arindam Bala <78309257+arindambala@users.noreply.github.com>
2021-02-06 19:10:44 +0530

    upload

---
+++ PASSWORD.py
@@ -0,0 +1,11 @@
+import random
+import string
+
+def get_random_password_string(length):
+   password_characters = string.ascii_letters + string.digits + string.punctuation
+
+   password = ''.join(random.choice(password_characters) for i in range(length))
+
+   print ("Your Password Is: ", password)
+
+get_random_password_string(10)
