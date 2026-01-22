8d30bf2561fc528b9e11de633f8d0cc36ae8ede2
arindambala <252558374+arindambala@users.noreply.github.com>
2026-01-22 22:25:02 +0530

    Day 1

---
+++ Udemy/Variables.py
@@ -0,0 +1,9 @@
+# Day 1 - 100 Days of Code
+
+# Band Name Generator
+print(f"\n---- Welcome to Band Name Generator! ----")
+
+past_lives = input("\nWhich city did you grow up in? : ")
+my_pet = input("What's thy pet's name? : ")
+
+print(f"\n____ Suggested Band Name : {past_lives} {my_pet} ! ____")
