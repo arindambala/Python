58c2bdff19eb9078309e72f15a90b1a5b2c4a0db
arindambala <252558374+arindambala@users.noreply.github.com>
2026-02-07 22:49:04 +0530

    Day 10

--- Udemy/Outputs.py
+++ Udemy/Outputs.py
@@ -4,10 +4,13 @@
 print(f"\n---- Welcome to the Calculator Application! ----")
 
 def format_name(first_name, last_name): # title()
+    if first_name == "" or last_name == "":
+        return
+    
     formatted_first_name = first_name.title()
     formatted_last_name = last_name.title()
-    
-    return f"{formatted_first_name} {formatted_last_name}"
+    return f"\nResult : {formatted_first_name} {formatted_last_name}" # EOF
+    # print("This isn't returned!") # Won't be executed
 
-formatted_name = format_name("ight", "BET")
+formatted_name = format_name(input("\nThe first name : "), input("\nThe last name : ")) # format_name("ight", "BET")
 print(formatted_name)
