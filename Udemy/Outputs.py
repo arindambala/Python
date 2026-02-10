8f5d51032317b21fed1bb790326a9c1510051954
arindambala <252558374+arindambala@users.noreply.github.com>
2026-02-10 18:18:55 +0530

    Day 10

--- Udemy/Outputs.py
+++ Udemy/Outputs.py
@@ -1,8 +1,5 @@
 # Day 10 - 100 Days of Code
 
-# Calculator
-# print(f"\n---- Welcome to the Calculator Application! ----")
-
 def format_name(first_name, last_name): # title()
     """A function that takes the first & last name, inputted by a user and returns the appropriate formatted name""" # Docstring
     if first_name == "" or last_name == "":
@@ -15,3 +12,18 @@ def format_name(first_name, last_name): # title()
 
 formatted_name = format_name(input("\nThe first name : "), input("\nThe last name : ")) # format_name("ight", "BET")
 print(formatted_name)
+
+def add(n1, n2):
+    return n1 + n2
+
+# def sub(n1, n2):
+#     return n1 - n2
+
+# def mul(n1, n2):
+#     return n1 * n2
+
+# def div(n1, n2):
+#     return n1 / n2
+
+my_favourite_op = add
+print(my_favourite_op(2, 3))
