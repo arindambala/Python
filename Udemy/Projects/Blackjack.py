086fff034a8f47502cbd039919483e9f2bf5e307
arindambala <252558374+arindambala@users.noreply.github.com>
2026-02-27 19:04:37 +0530

    Day 12

--- Udemy/Projects/Blackjack.py
+++ Udemy/Projects/Blackjack.py
@@ -7,7 +7,7 @@ print(f"\n---- Welcome to Blackjack! ----\n")
 print("\n Card - Rank : ")
 
 # Use a dictionary - Card Values w.r.t Ranks
-dict = {'A': 10, 'K' : 10, 'Q' : 10, 'J' : 10} # Face cards // Other cards | Same as the number
+dict = {'A': 10, 'K' : 10, 'Q' : 10, 'J' : 10} # Face cards // Other cards | Same as the number - {1, 2, 3,...}
 
 # continued : Check the code with - 'class'
 # commit - Twenty One | FreeCodeCamp - YouTube
