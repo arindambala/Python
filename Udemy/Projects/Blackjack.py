cd29e5b139d619d838f75b05a1bd181798d6888c
arindambala <252558374+arindambala@users.noreply.github.com>
2026-03-06 20:33:19 +0530

    Day 12

--- Udemy/Projects/Blackjack.py
+++ Udemy/Projects/Blackjack.py
@@ -12,7 +12,7 @@ dict = {'A': 10, 'K' : 10, 'Q' : 10, 'J' : 10} # Face cards // Other cards | Sam
 # With 'class' | Without 'class' - Already committed on other program
 
 play = 0 # Rank - Value ? Player : Dealer
-play_count = 0, dealer_play = 0, dealer_hold = 0
+play_count = 0, dealer_play = 0, dealer_hold = 0 # Count number of times play took place also
 
 # continued : Check the code with - 'class'
 # commit - Twenty One | FreeCodeCamp - YouTube
