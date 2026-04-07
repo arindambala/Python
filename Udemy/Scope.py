cf44d01601606129862358b18c1ec14f670ef625
arindambala <252558374+arindambala@users.noreply.github.com>
2026-04-07 20:45:53 +0530

    Day 13

---
+++ Udemy/Scope.py
@@ -0,0 +1,17 @@
+# Day 13 - 100 Days of Code
+
+import random
+
+# GUESSNUMLOGO = '''
+#     ____ __   __ ___  ___ ___
+#     / _` | | | |/ _ \/ __/ __|
+#     | (_| | |_| |  __/\__ \__ \
+#     \__, |\__,_|\___||___/___/
+#     __/ |                    
+#     |___/     '''
+
+# print(GUESSNUMLOGO)
+
+# Guess the number
+print(f"\n---- Welcome to Guess the Number! ----")
+
