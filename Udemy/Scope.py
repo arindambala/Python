deefbb4713bf705cfc550b0f29503e9dc0ba27c0
arindambala <252558374+arindambala@users.noreply.github.com>
2026-04-08 00:06:33 +0530

    Day 13

--- Udemy/Scope.py
+++ Udemy/Scope.py
@@ -1,6 +1,10 @@
 # Day 13 - 100 Days of Code
 
-import random
+from random import randint
+
+# Constants
+EASY_LVL_TURNS = 5
+HARD_LVL_TURNS = 3
 
 # GUESSNUMLOGO = '''
 #     ____ __   __ ___  ___ ___
@@ -10,8 +14,45 @@ import random
 #     __/ |                    
 #     |___/     '''
 
-# print(GUESSNUMLOGO)
-
 # Guess the number
 print(f"\n---- Welcome to Guess the Number! ----")
 
+def check_guess(player_guess, actual_guess, turns):
+    ''' Checks num against guess & returns remaining number of turns '''
+    if player_guess > actual_guess:
+        print("_ HIGH. Please RETRY with a LOWER number! _")
+        return turns - 1
+    elif player_guess < actual_guess:
+        print("_ LOW. Please RETRY with a HIGHER number! _")
+        return turns - 1
+    else:
+        print(f"\n|> LET'S GO! The GUESS was {actual_guess} ! CONGRATULATIONS! \n")
+
+def set_diff():
+    lvl = input("\nDifficulty of the game? (Easy | Hard) : ").lower()
+    if lvl == 'easy':
+        return EASY_LVL_TURNS
+    else:
+        return HARD_LVL_TURNS
+
+def game():
+    # print(GUESSNUMLOGO)
+    print(f"\n---- What number between 1 & 100 has been thought of? ----\n")
+    num = randint(1, 100)
+    print(f"The guessed number : {num}")
+
+    turns = set_diff()
+
+    guess = 0
+    while guess != num:
+        print(f"\nAttempts remaining : {turns}")
+
+        guess = int(input("\nPlayer guessed : "))
+
+        turns = check_guess(guess, num, turns)
+
+        if turns == 0:
+            print(f"\n|> LOST! The GUESS was {num} ! OUT OF GUESSES! \n")
+            return
+
+game()
