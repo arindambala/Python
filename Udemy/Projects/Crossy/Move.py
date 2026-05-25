933fc3e6c6f69e4d1205142ccc8f55faea77f445
arindambala <252558374+arindambala@users.noreply.github.com>
2026-05-25 22:14:44 +0530

    Day 23

---
+++ Udemy/Projects/Crossy/Move.py
@@ -0,0 +1,26 @@
+COLORS = ['violet', 'indigo', 'blue', 'green', 'yellow', 'orange', 'red']
+START_MOVE_DISTANCE = 5
+MOVE_INCREMENT = 10
+
+from turtle import Turtle
+import random
+
+class Cars:
+    def __init__(self):
+        self.cars = []
+    
+    def create_car(self):
+        chance = random.randint(1, 6)
+        if chance == 1:
+            car = Turtle('square')
+            car.shapesize(stretch_wid=1, stretch_len=2)
+            car.penup()
+            car.color(random.choice(COLORS))
+            
+            pos_y = random.randint(-250, 250)
+            car.goto(300, pos_y)
+            self.cars.append(car)
+    
+    def move_cars(self):
+        for car in self.cars:
+            car.backward(START_MOVE_DISTANCE)
---
+++ Udemy/Projects/Crossy/Turt.py
@@ -0,0 +1,18 @@
+START_POSITION = (0, -280)
+MOVE_DISTANCE = 10
+FINISH_LINE = 280
+
+from turtle import Turtle
+
+class Player(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape('turtle')
+        self.speed('fastest')
+        self.color('DarkOrchid')
+        self.penup()
+        self.goto(START_POSITION)
+        self.setheading(90)
+    
+    def move_play(self):
+        self.forward(MOVE_DISTANCE)
