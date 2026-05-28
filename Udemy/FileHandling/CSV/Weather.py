4d6aa674e877634de7ed3c5e7f254e062a5803b5
arindambala <252558374+arindambala@users.noreply.github.com>
2026-05-28 23:22:24 +0530

    Day 25

--- Udemy/FileHandling/CSV/Weather.py
+++ Udemy/FileHandling/CSV/Weather.py
@@ -1,2 +1,19 @@
 # Day 25 - 100 Days of Code
 
+import csv
+
+print(f"\n---- Weather ^ Data ----\n")
+
+# with open('Weather_Data.csv') as file:
+#     data = file.readlines()
+#     print(data)
+
+with open('Weather_Data.csv') as file: # So much faff!
+    data = csv.reader(file)
+    temperatures = []
+    # print(data)
+    for row in data:
+        # print(row)
+        if row[1] != 'TEMPERATURE':
+            temperatures.append(int(row[1]))
+    print(temperatures)
