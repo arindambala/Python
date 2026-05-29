ca78e71171a6d173433a6bb0dc5a196f202090f8
arindambala <252558374+arindambala@users.noreply.github.com>
2026-05-29 22:38:07 +0530

    Day 25

--- Udemy/FileHandling/CSV/Weather.py
+++ Udemy/FileHandling/CSV/Weather.py
@@ -20,5 +20,39 @@ print(f"\n---- Weather ^ Data ----\n")
     print(temperatures) '''
 
 data = pandas.read_csv('Weather_Data.csv')
+# print(data) - type() | DataFrame(2-D)
+# print(data['TEMPERATURE']) type() | Series(1-D)
+
+# print(data.to_dict()) - DataFrame to Dictionary
+# print(data['TEMPERATURE'].to_list()) - Series to list
+
+''' temp_list = data['TEMPERATURE'].to_list()
+tot = 0
+for temp in temp_list:
+    tot += temp
+avg_temp = tot / len(temp_list)
+print(f"\nAverage Temperature | (Weekly) : {avg_temp} Celsius!") '''
+
+# print(data['TEMPERATURE'].mean())
+# print(data['TEMPERATURE'].max())
+
+''' # Data - Columns
+print(data['CONDITION'])
+print(data.CONDITION) 
+
+# Data - Rows
+print(data[data.DAY == 'Monday'])
+print(data[data.TEMPERATURE == data.TEMPERATURE.max()]) 
+
+monday = data[data.DAY == 'Monday']
+print(monday.CONDITION)
+print(monday.TEMPERATURE[0] * (9 / 5) + 32) '''
+
+# DataFrame from Scratch
+dict = {
+    'students': ['Ed', 'Edd', 'Eddy'],
+    'scores': [75, 50, 95]
+}
+data = pandas.DataFrame(dict)
 # print(data)
-print(data['TEMPERATURE'])
+data.to_csv('Ops.csv')
