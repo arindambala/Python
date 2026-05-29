6876c6cfa0da4f912291aa0cd491eaa86ab39d15
arindambala <252558374+arindambala@users.noreply.github.com>
2026-05-29 23:39:31 +0530

    Day 25

--- Udemy/FileHandling/CSV/Weather.py
+++ Udemy/FileHandling/CSV/Weather.py
@@ -19,7 +19,7 @@ print(f"\n---- Weather ^ Data ----\n")
             temperatures.append(int(row[1]))
     print(temperatures) '''
 
-data = pandas.read_csv('Weather_Data.csv')
+# data = pandas.read_csv('Weather_Data.csv')
 # print(data) - type() | DataFrame(2-D)
 # print(data['TEMPERATURE']) type() | Series(1-D)
 
@@ -46,13 +46,35 @@ print(data[data.TEMPERATURE == data.TEMPERATURE.max()])
 
 monday = data[data.DAY == 'Monday']
 print(monday.CONDITION)
-print(monday.TEMPERATURE[0] * (9 / 5) + 32) '''
+print(monday.TEMPERATURE[0] * (9 / 5) + 32) 
 
-# DataFrame from Scratch
+DataFrame from Scratch
 dict = {
     'students': ['Ed', 'Edd', 'Eddy'],
     'scores': [75, 50, 95]
 }
 data = pandas.DataFrame(dict)
 # print(data)
-data.to_csv('Ops.csv')
+data.to_csv('Ops.csv') '''
+
+# Central Park Squirrel Count NYC 2018
+data = pandas.read_csv('2018_Central_Park_Squirrel_Census_NYC.csv')
+
+grey_squirrels_count = len(data[data['Primary Fur Color'] == 'Gray'])
+print(grey_squirrels_count)
+
+cinnamon_squirrels_count = len(data[data['Primary Fur Color'] == 'Cinnamon'])
+print(cinnamon_squirrels_count)
+
+black_squirrels_count = len(data[data['Primary Fur Color'] == 'Black'])
+print(black_squirrels_count)
+
+# squirrels_count = grey_squirrels_count + cinnamon_squirrels_count + black_squirrels_count
+# print(squirrels_count)
+
+dict = {
+    'Fur Color': ['Gray', 'Cinnamon', 'Black'],
+    'Count': [grey_squirrels_count, cinnamon_squirrels_count, black_squirrels_count]
+}
+squirrels_count = pandas.DataFrame(dict)
+squirrels_count.to_csv('squirrel_count.csv')
