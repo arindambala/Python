4c9861535a13b5430b362570559bb079ffb38d24
arindambala <252558374+arindambala@users.noreply.github.com>
2026-05-30 19:28:13 +0530

    Day 25

--- Udemy/Projects/StateGuess/Guess.py
+++ Udemy/Projects/StateGuess/Guess.py
@@ -1,6 +1,7 @@
 # Day 25 - 100 Days of Code
 
 import turtle
+import pandas
 
 screen = turtle.Screen()
 screen.title('U.S. States Guess')
@@ -10,7 +11,31 @@ screen.addshape(img)
 
 turtle.shape(img)
 
-guess = screen.textinput(title='Guess the State!', prompt='Which state to be found?')
-print(guess)
+data = pandas.read_csv('50_states.csv')
+states = data.state.to_list()
+states_guessed = []
 
-screen.exitonclick()
+while len(states_guessed) < 50:
+    guess = screen.textinput(title=f'{len(states_guessed)}/50 States', prompt='Which state to be found?').title()
+    # print(guess)
+
+    if guess == 'Exit':
+        states_missed = []
+        for state in states:
+            if state not in states_guessed:
+                states_missed.append(state)
+        # print(states_missed)
+        learn = pandas.DataFrame(states_missed)
+        learn.to_csv('learn_states.csv')
+        break
+    
+    if guess in states:
+        states_guessed.append(guess)
+        bloom = turtle.Turtle()
+        bloom.hideturtle()
+        bloom.penup()
+        
+        state_data = data[data.state == guess]
+        bloom.goto(state_data.x.item(), state_data.y.item())
+        # bloom.write(state_data.state.item())
+        bloom.write(guess)
