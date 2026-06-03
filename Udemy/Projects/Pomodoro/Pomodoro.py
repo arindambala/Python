b5bef16ef9db8f6fea541ba5effcecc51a6524ef
arindambala <252558374+arindambala@users.noreply.github.com>
2026-06-03 23:09:01 +0530

    Day 28

--- Udemy/Projects/Pomodoro/Pomodoro.py
+++ Udemy/Projects/Pomodoro/Pomodoro.py
@@ -2,10 +2,11 @@
 
 from tkinter import *
 
-PINK = ''
-RED = ''
-GREEN = ''
-YELLOW = ''
+PINK = '#FFB5A7'
+RED = '#E63946'
+GREEN = '#A8DADC'
+YELLOW = '#FFD166'
+BG = '#F8F9FA'
 
 FONT_NAME = 'Courier'
 WORK = 25
@@ -22,6 +23,7 @@ window.config(padx=100, pady=50)
 canvas = Canvas(width=200, height=224) # Canvas Widget
 img = PhotoImage(file='tomato.png') # Holds an image at a file location
 canvas.create_image(102, 112, image = img)
+canvas.create_text(102, 130, text='00:00', fill='white', font=(FONT_NAME, 30, 'bold'))
 canvas.pack()
 
 window.mainloop()
