5777748eabd4885a3044c582221804b345384924
arindambala <252558374+arindambala@users.noreply.github.com>
2026-06-03 23:18:41 +0530

    Day 28

--- Udemy/Projects/Pomodoro/Pomodoro.py
+++ Udemy/Projects/Pomodoro/Pomodoro.py
@@ -6,7 +6,6 @@ PINK = '#FFB5A7'
 RED = '#E63946'
 GREEN = '#A8DADC'
 YELLOW = '#FFD166'
-BG = '#F8F9FA'
 
 FONT_NAME = 'Courier'
 WORK = 25
@@ -18,12 +17,12 @@ print(f"\n---- Pomodoro ^ Timer ----\n")
 # -------- UI Setup --------
 window = Tk()
 window.title('Pomodoro')
-window.config(padx=100, pady=50)
+window.config(padx=100, pady=50, bg=YELLOW)
 
-canvas = Canvas(width=200, height=224) # Canvas Widget
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0) # Canvas Widget
 img = PhotoImage(file='tomato.png') # Holds an image at a file location
-canvas.create_image(102, 112, image = img)
-canvas.create_text(102, 130, text='00:00', fill='white', font=(FONT_NAME, 30, 'bold'))
+canvas.create_image(100, 112, image = img)
+canvas.create_text(100, 130, text='00:00', fill='white', font=(FONT_NAME, 30, 'bold'))
 canvas.pack()
 
 window.mainloop()
