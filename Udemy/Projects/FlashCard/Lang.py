164f2beb68d6ff18f7687ba5cdd67ded7f8afcc1
arindambala <252558374+arindambala@users.noreply.github.com>
2026-06-18 23:05:39 +0530

    Day 31

--- Udemy/Projects/FlashCard/Lang.py
+++ Udemy/Projects/FlashCard/Lang.py
@@ -15,6 +15,8 @@ learn = df.to_dict(orient='records') # Orients the table to create the dictionar
 def nxt_card():
     curr_card = random.choice(learn)
     # print(curr_card['French'])
+    canvas.itemconfig(card_title, text='French')
+    canvas.itemconfig(card_word, text=curr_card['French'])
 
 window = Tk()
 window.title("Vive l'Angleterre - Vive la France")
@@ -23,8 +25,8 @@ window.config(padx=50, pady=50, bg=BG_COLOR)
 canvas = Canvas(width=800, height=526, bg=BG_COLOR, highlightthickness=0)
 cf_img = PhotoImage(file='images/card_front.png')
 canvas.create_image(400, 263, image = cf_img)
-canvas.create_text(400, 150, text='Title', font=('Ariel', 40, 'italic'))
-canvas.create_text(400, 263, text='word', font=('Ariel', 60, 'bold'))
+card_title = canvas.create_text(400, 150, text='Title', font=('Ariel', 40, 'italic'))
+card_word = canvas.create_text(400, 263, text='word', font=('Ariel', 60, 'bold'))
 canvas.grid(row=0, column=0, columnspan=2)
 
 wrg_img = PhotoImage(file='images/wrong.png')
@@ -34,5 +36,7 @@ unknown_Button.grid(row=1, column=0)
 rgt_img = PhotoImage(file='images/right.png')
 known_Button = Button(image=rgt_img, highlightthickness=0, command=nxt_card)
 known_Button.grid(row=1, column=1)
+
+nxt_card()
 
 window.mainloop()
