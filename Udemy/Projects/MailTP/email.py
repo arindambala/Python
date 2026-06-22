e75ae05feec2f1d4df0310b1169b39326145e605
arindambala <252558374+arindambala@users.noreply.github.com>
2026-06-22 23:04:37 +0530

    Day 32

--- Udemy/Projects/MailTP/email.py
+++ Udemy/Projects/MailTP/email.py
@@ -3,6 +3,14 @@
 import smtplib
 
 mail = '_address_@gmail.com'
+key = '_!@#$%^&*()+_'
 
-connect = smtplib.SMTP('smtp.gmail.com') # Depends on the information about the particular domain
-connect.starttls()
+connection = smtplib.SMTP('smtp.gmail.com') # Depends on the information about the particular domain
+
+with connection as connect:
+    connect.starttls()
+
+    connect.login(user=mail, password=key)
+    connect.sendmail(from_addr=mail, to_addrs='_address_@live.com', msg='Subject:Hello\n\nBruh')
+
+    #connect.close()
