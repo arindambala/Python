eb5630ab752d544c38ad9db018977b9a19da376b
arindambala <252558374+arindambala@users.noreply.github.com>
2026-06-28 22:29:04 +0530

    Day 33

--- Udemy/API.py
+++ Udemy/API.py
@@ -6,3 +6,11 @@ print(f"\n---- ISS ^ Locator ----\n")
 
 response = requests.get(url='http://api.open-notify.org/iss-now.json')
 # print(response)
+
+'''
+1XX - Hold On
+2XX - Here You G
+3XX - Go Away
+4XX - You Screwed Up
+5XX - I Screwed Up
+'''
