4fa1f66d2e1345d4abfc445d9390beb213ccafe9
arindambala <252558374+arindambala@users.noreply.github.com>
2026-06-29 20:19:05 +0530

    Day 33

--- Udemy/API.py
+++ Udemy/API.py
@@ -5,7 +5,7 @@ import requests
 print(f"\n---- ISS ^ Locator ----\n")
 
 response = requests.get(url='http://api.open-notify.org/iss-now.json')
-# print(response)
+print(response.status_code) # print(response)
 
 '''
 1XX - Hold On
@@ -14,3 +14,8 @@ response = requests.get(url='http://api.open-notify.org/iss-now.json')
 4XX - You Screwed Up
 5XX - I Screwed Up
 '''
+
+if response.status_code == 404:
+    raise Exception('Does not exist!')
+elif response.status_code == 401:
+    raise Exception('Unauthorised access!')
