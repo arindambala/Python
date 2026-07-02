4d68c1d8981f6936fc843bf5d54c0003b2baff1e
arindambala <252558374+arindambala@users.noreply.github.com>
2026-07-02 20:03:57 +0530

    Day 33

--- Udemy/API.py
+++ Udemy/API.py
@@ -1,11 +1,16 @@
 # Day 33 - 100 Days of Code
 
+LAT = 20.296059
+LLT = 85.824539
+
+from datetime import datetime
+from zoneinfo import ZoneInfo
 import requests
 
 print(f"\n---- ISS ^ Locator ----\n")
 
 response = requests.get(url='http://api.open-notify.org/iss-now.json')
-print(response.status_code) # print(response)
+# print(response.status_code) | print(response)
 
 '''
 1XX - Hold On
@@ -25,8 +30,33 @@ response.raise_for_status()
 data = response.json()['iss_position'] # Pinpoint
 # print(data)
 
-lon = data['longitude']
-lat = data['latitude']
+lon = float(data['longitude'])
+lat = float(data['latitude'])
 
-iss_pos = (lon, lat)
-print(iss_pos)
+# iss_pos = (lon, lat)
+# print(iss_pos)
+
+parameters = {
+    'lat': LAT,
+    'lng': LLT,
+    'formatted': 0,
+}
+
+response = requests.get('https://api.sunrise-sunset.org/json', params=parameters)
+response.raise_for_status()
+
+data = response.json()
+# print(data)
+
+local_tz = ZoneInfo('Asia/Kolkata')
+
+sunrise = data['results']['sunrise']
+sunset = data['results']['sunset']
+
+sunrise_utc = datetime.fromisoformat(sunrise)
+sunset_utc = datetime.fromisoformat(sunset)
+
+sunrise_ist = sunrise_utc.astimezone(local_tz)
+sunset_ist = sunset_utc.astimezone(local_tz)
+
+curr_time = datetime.now(local_tz)
