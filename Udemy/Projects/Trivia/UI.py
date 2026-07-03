b92b2a465b2239fe23acce37b1cd02c18809be8c
arindambala <252558374+arindambala@users.noreply.github.com>
2026-07-03 21:40:30 +0530

    Day 34

--- Udemy/Projects/Trivia/UI.py
+++ Udemy/Projects/Trivia/UI.py
@@ -1,1 +1,10 @@
 THEME_COLOUR = '#253A45'
+
+from tkinter import *
+
+class Interface:
+    def __init__(self):
+        self.window = Tk()
+        self.window.title('Factos')
+        
+        self.window.mainloop()
