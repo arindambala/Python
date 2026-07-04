c15bbad1de74d7e383341eca9fbede629fa83860
arindambala <252558374+arindambala@users.noreply.github.com>
2026-07-04 22:38:09 +0530

    Day 34

--- Udemy/Projects/Trivia/UI.py
+++ Udemy/Projects/Trivia/UI.py
@@ -1,9 +1,12 @@
 THEME_COLOUR = '#253A45'
 
 from tkinter import *
+from Func import Task
 
 class Interface:
-    def __init__(self):
+    def __init__(self, func: Task):
+        self.quiz = func
+        
         self.window = Tk()
         self.window.title('Factos')
         self.window.config(padx=20, pady=20, bg=THEME_COLOUR)
@@ -12,7 +15,7 @@ class Interface:
         self.score.grid(row=0, column=1)
         
         self.canvas = Canvas(width=300, height=250, bg='white')
-        self.ques_text = self.canvas.create_text(150, 125, text='Some Text', fill=THEME_COLOUR, font=('Arial', 20, 'italic'))
+        self.ques_text = self.canvas.create_text(150, 125, width=280, text='Some Text', fill=THEME_COLOUR, font=('Arial', 20, 'italic'))
         self.canvas.grid(row=1, column=0, columnspan=2, pady=50)
         
         self.true_img = PhotoImage(file='images/true.png')
@@ -23,4 +26,10 @@ class Interface:
         self.false_button = Button(image=self.false_img, highlightthickness=0, bd=0, bg=THEME_COLOUR, activebackground=THEME_COLOUR)
         self.false_button.grid(row=2, column=1)
         
+        self.get_next_ques()
+        
         self.window.mainloop()
+    
+    def get_next_ques(self):
+        q_text = self.quiz.next_question()
+        self.canvas.itemconfig(self.ques_text, text=q_text)
