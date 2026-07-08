9cb10ef685352496898f85b7cb4af5cdff8db57f
arindambala <252558374+arindambala@users.noreply.github.com>
2026-07-08 20:53:59 +0530

    Day 37

--- Udemy/Projects/Tracker/streak.py
+++ Udemy/Projects/Tracker/streak.py
@@ -1,9 +1,47 @@
 # Day 37 - 100 Days of Code
 
+import os
 import requests
+from dotenv import load_dotenv
 
-print(f'\n---- Habit ^ Tracker ----\n')
+env_path = '/path/to/the/variable/folder/.env'
+load_dotenv(dotenv_path=env_path)
 
 ''' HTTP Requests - GET / PUT / POST / DELETE '''
 
-response = requests.get('')
+TOKEN = os.environ.get('TOKEN')
+USERNAME = os.environ.get('USERNAME')
+
+PIXELA_ENDPOINT = 'https://pixe.la/v1/users'
+GRAPH_ENDPOINT = f'{PIXELA_ENDPOINT}/{USERNAME}/graphs'
+
+print(f'\n---- Habit ^ Tracker ----\n')
+
+''' CREATE YOUR USER ACCOUNT '''
+
+user_parameters = {
+    'token': TOKEN,
+    'username': USERNAME,
+    'agreeTermsOfService': 'yes',
+    'notMinor': 'yes',
+}
+
+response = requests.post(url=PIXELA_ENDPOINT, json=user_parameters)
+print(response.text)
+
+''' CREATE A GRAPH DEFINITION'''
+
+graph_config = {
+    'id': 'dayW',
+    'name': 'WalkWalk',
+    'unit': 'Km',
+    'type': 'float',
+    'color': 'ajisai'
+}
+
+header = {
+    'X-USER-TOKEN': TOKEN
+}
+
+response = requests.post(url=GRAPH_ENDPOINT, json=graph_config, headers=header)
+print(response.text)
