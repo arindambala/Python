aa46ed5455733d9dbdddd90e99837fef68634ff3
arindambala <252558374+arindambala@users.noreply.github.com>
2026-07-11 00:00:19 +0530

    Day 40

--- Udemy/Projects/FlightDeal/Func.py
+++ Udemy/Projects/FlightDeal/Func.py
@@ -9,12 +9,14 @@ class Task:
     def __init__(self):
         self.api_endpoint = os.environ.get('SHEETY_ENDPOINT')
         self.api_key = os.environ.get('BEARER_KEY')
+        self.user_endpoint = os.environ.get('USER_ENDPOINT')
         
         self.auth = {
             'Authorization': f'Bearer {self.api_key}'
         }
         
         self.destination_data = {}
+        self.customer_data = {}
     
     def get_destination_data(self):
         response = requests.get(url=self.api_endpoint, headers=self.auth)
@@ -32,3 +34,12 @@ class Task:
             }
         }
         requests.put(url=f'{self.api_endpoint}/{row_id}', json=updated_data, headers=self.auth)
+    
+    def get_customer_emails(self):
+        response = requests.get(url=self.user_endpoint, headers=self.auth)
+        
+        data = response.json()
+        #print(data)
+        
+        self.customer_data = data['users']
+        return self.customer_data
