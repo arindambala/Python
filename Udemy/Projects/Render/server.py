8b7ef1a6a01161587d54746cf93ab4d574d4534a
arindambala <252558374+arindambala@users.noreply.github.com>
2026-07-20 23:26:38 +0530

    Day 56

--- Udemy/Projects/Render/server.py
+++ Udemy/Projects/Render/server.py
@@ -1,23 +1,14 @@
 # Day 56 - 100 Days of Code
 
-import os
 from flask import Flask, render_template
-from dotenv import load_dotenv
-
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-env_path = os.path.join(BASE_DIR, '.env')
-load_dotenv(dotenv_path=env_path)
 
 print('\n---- Static ^ Templates ----\n')
 
 app = Flask(__name__)
 
-GIPHY = os.getenv('WEB')
-SITE = os.getenv('TEMPLATE')
-
 @app.route('/')
 def home():
-    return render_template('site.html', web_link=GIPHY, web_site=SITE)
+    return render_template('site.html')
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=False)
